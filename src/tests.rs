321fe5cdd9bb1216349e83fb8abb9be3a88e073b
AlphaModder <quasiflux@gmail.com>
2018-11-15 16:17:53 -0800

    Update tests to use new API.

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,20 @@
 extern crate tokio_core;
-use self::tokio_core::Core;
+extern crate native_tls;
+extern crate tokio_tls;
+
+use std::sync::Arc;
+use std::io;
+use std::str::FromStr;
+
+use futures::{future, Future};
+use hyper::Uri;
+use hyper::client::{Client, Connect, Service, HttpConnector};
+
+use self::tokio_core::reactor::Core;
+use self::tokio_core::net::TcpStream;
+use self::tokio_tls::TlsStream;
+use self::tokio_tls::TlsConnectorExt;
+use self::native_tls::TlsConnector;
 
 use super::*;
 
@@ -9,15 +24,50 @@ const TEST_SUBJECT: &str = "Something to Remember";
 const TEST_BODY: &str = "Always, somewhere, someone is fighting for you.";
 const TEST_TIMESTAMP: &str = "2014-06-28 16:05";
 
-fn create_client() -> (Core, SpaceEmailClient) {
-    let mut core = Core::new().unwrap();
-    let client = hyper::Client::new(&core.handle());
-    SpaceEmailClient::new(client)
+struct HttpsConnector {
+    tls: Arc<TlsConnector>,
+    http: HttpConnector,
+}
+
+impl Service for HttpsConnector {
+    type Request = Uri;
+    type Response = TlsStream<TcpStream>;
+    type Error = io::Error;
+    type Future = Box<Future<Item = Self::Response, Error = io::Error>>;
+
+    fn call(&self, uri: Uri) -> Self::Future {
+        if uri.scheme() != Some("https") {
+            return future::err(io::Error::new(io::ErrorKind::Other, "only works with https")).boxed()
+        }
+
+        let host = match uri.host() {
+            Some(s) => s.to_string(),
+            None => return future::err(io::Error::new(io::ErrorKind::Other, "missing host")).boxed()
+        };
+
+        let tls_cx = self.tls.clone();
+        Box::new(self.http.call(uri).and_then(move |tcp| {
+            tls_cx.connect_async(&host, tcp).map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+        }))
+    }
+}
+
+fn create_client() -> (Core, SpaceEmailClient<HttpsConnector>) {
+    let core = Core::new().unwrap();
+    let tls_cx: TlsConnector = native_tls::TlsConnector::builder().unwrap().build().unwrap();
+    let mut connector = HttpsConnector {
+        tls: Arc::new(tls_cx),
+        http: HttpConnector::new(2, &core.handle()),
+    };
+    connector.http.enforce_http(false);
+    let client = hyper::Client::configure().connector(connector).build(&core.handle());
+    (core, SpaceEmailClient::new(client))
 }
 
 #[test]
 fn get_email() {
-    let email = create_client().1.get_id(TEST_ID).unwrap();
+    let (mut core, client) = create_client();
+    let email = core.run(client.get_by_id(TEST_ID)).unwrap();
     assert_eq!(email.contents().sender, TEST_SENDER);
     assert_eq!(email.contents().subject, TEST_SUBJECT);
     assert_eq!(email.contents().body, TEST_BODY);
@@ -26,17 +76,19 @@ fn get_email() {
 
 #[test]
 fn get_random() {
-    create_client().1.get_random().unwrap();
+    let (mut core, client) = create_client();
+    core.run(client.get_random()).unwrap();
 }
 
 #[test]
 fn send_email() {
-    create_client().1.send(&SpaceEmailContents {
+    let (mut core, client) = create_client();
+    core.run(client.send(&SpaceEmailContents {
         sender: TEST_SENDER.to_string(),
         subject: TEST_SUBJECT.to_string(),
         body: TEST_BODY.to_string(),
-        color: SpaceEmailColor::Yellow,
-    }).unwrap();
+        style: SpaceEmailStyle::Yellow,
+    })).unwrap();
 }
 
 
